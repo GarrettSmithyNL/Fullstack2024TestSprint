6e10f4498b3e8958cc1fb368cf95276f1eb9e146
GarrettSmithyNL <garrett_smith_30@hotmail.com>
2024-05-28 14:05:53 -0230

    basic CLI implemented

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 global.DEBUG = true;
 
 const fs = require("fs");
+
+const myArgs = process.argv.slice(2);
+
+if (DEBUG && myArgs.length >= 1) console.log("My Args: " + myArgs);
+
+switch (myArgs[0]) {
+  case "init":
+  case "i":
+    if (DEBUG) console.log(myArgs[0] + " - init the app.");
+    break;
+  case "config":
+  case "c":
+    if (DEBUG)
+      console.log(myArgs[0] + " - create the config folder and files.");
+    break;
+  case "token":
+  case "t":
+    if (DEBUG) console.log(myArgs[0] + " - manage the tokens.");
+    break;
+  case "--help":
+  case "--h":
+  default:
+    // Read the help file
+    if (DEBUG) console.log(myArgs[0] + " - display help");
+    break;
+}
